d0422124fd6cf84088336e49f4a212556a50fd4f
recenama <cajilemapaola@gmail.com>
2024-06-26 22:20:22 -0500

    Kafka producer with callback

---
+++ kafka-basics/src/main/java/io/demos/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,58 @@
+package io.demos.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello world");
+
+        // connect to local host
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create a producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //create a Producer Record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+        //send data
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                // executes every time a record successfully sent or an exception is thrown
+                if (exception == null) {
+                    // the record was successfully sent
+                    log.info("Received new metadata \n" +
+                            "Topic: " + metadata.topic() + "\n" +
+                            "Partition: " + metadata.partition() + "\n" +
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp());
+
+                } else {
+                    log.error("Error while producing", exception);
+                }
+            }
+        });
+
+        // tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+    }
+}
