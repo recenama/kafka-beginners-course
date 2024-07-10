e8a8138240e0d27844cf4a8c1a2b55ef09097556
recenama <cajilemapaola@gmail.com>
2024-07-10 17:36:08 -0500

    Implement a kafka producer with information from wikimedia stream

---
+++ kafka-producer-wikimedia/src/main/java/io/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,39 @@
+package io.demos.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+
+    public static void main(String[] args) throws InterruptedException {
+        // connect to local host
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","127.0.0.1:9092");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer",StringSerializer.class.getName());
+
+        // create a producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        String topic ="wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer,topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        // start producer in another thread
+        eventSource.start();
+
+        //we produce for 10 minutes  and block the program until then
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
